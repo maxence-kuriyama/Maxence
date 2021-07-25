840e45a35a77068f016be166db203b00759b5719
maxence-kuriyama <maxence.no1@gmail.com>
2021-07-25 21:11:27 +0900

    refactor: エンディング関連メッセージをインクルードファイルに

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "field.h"
 #include "fireflower.h"
 #include "scenario.h"
+#include "ending.h"
 using namespace DxLib;
 using namespace std;
 using namespace Eigen;
@@ -246,22 +247,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	int end_cnt = 0;
 	string job[20];
 	string who[20];
-	job[0] = "企画";				who[0] = "兼 総監督 Mr.A";
-	job[1] = "原案・脚本";		who[1] = "想像力の権化\n Mr.A & Mr.O";
-	job[2] = "キャラクター\n デザイン";	who[2] = "未来の世界の\n クリエイター\n  Mr.O & Mr.A";
-	job[3] = "グラフィック";		who[3] = "既成概念など知らぬ\n Mr.O";
-	job[4] = "ドット絵";			who[4] = "激震のマウスカーソル\n Mr.T";
-	job[5] = "3Dモデル";			who[5] = "インテリジェンス\n デザイナー\n  Mr.O";
-	job[6] = "動画";				who[6] = "踊り狂う探究心\n Mr.O";
-	job[7] = "システム\n デザイン";	who[7] = "目先の仕事は\n 投げるもの\n  Mr.T";
-	job[8] = "プログラム";		who[8] = "量産型九龍城砦\n Mr.T";
-	job[9] = "音楽";				who[9] = "破壊と調和の境界線\n Mr.O & Mr.A";
-	job[10] = "セリフ";			who[10] = "唯一無二の有頂天\n Mr.A";
-	job[11] = "ゲーム\n デザイン";	who[11] = "悪乗りの玉手箱\n Mr.T & Mr.O";
-	job[12] = "タイトルロゴ\n デザイン";	who[12] = "すみませんでした\n Mr.T";
-	job[13] = "デバッグ\n テストプレイ";	who[13] = "Densuke Shiraishi";
-	job[14] = "スペシャル\n サンクス";		who[14] = "Maxence";
-	job[15] = "And YOU";		who[15] = "Thank you\n for playing!!";
+	init_ending_text(job, who);
 
 	//学習関連
 	int train_game_cnt = 0;
