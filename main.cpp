d636b4b9cad0d0cc13ea0994e1201fcb0a9a7b96
maxence-kuriyama <maxence.no1@gmail.com>
2021-09-06 01:19:02 +0900

    1ファイル化、メモリ125MB使用

--- main.cpp
+++ main.cpp
@@ -1,10 +1,287 @@
 #include "DxLib.h"
-#include <iostream>
 
 using namespace DxLib;
-#include "lib/game.h"
 
-#pragma comment(lib, "winmm.lib")
+class Mouse {
+public:
+	unsigned int button[8];
+	int x;
+	int y;
+
+	//マウス状態の取得
+	int update() {
+		if (GetMousePoint(&x, &y) == -1) {
+			return -1;
+		}
+		int MouseInput = GetMouseInput();
+		for (int i = 0; i < 8; i++) {
+			if (MouseInput & (1 << i)) {
+				button[i]++;
+			}
+			else {
+				button[i] = 0;
+			}
+		}
+		return 0;
+	}
+
+	bool click() {
+		return button[0] == 1;
+	}
+
+	bool onClick() {
+		return button[0] > 1;
+	}
+
+	bool clickRight() {
+		return button[1] == 1;
+	}
+
+	//矩形の上にマウスカーソルがいるか判定
+	bool onButton(int LeftUpx, int LeftUpy, int RightDownx, int RightDowny) {
+		return (x > LeftUpx && x < RightDownx&& y > LeftUpy && y < RightDowny);
+	}
+};
+
+
+class Field {
+private:
+	int filled() {
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				if (state[i][j] == 0) return 0;
+			}
+		}
+		return 1;
+	}
+
+public:
+	int state[3][3];	// 0:None, 1:Black, -1:White
+
+	Field() {
+		initialize();
+	}
+
+	void initialize() {
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				state[i][j] = 0;
+			}
+		}
+	}
+
+	int victory() {
+		int vict = 0;
+		int temp = 0;
+
+		for (int k = 0; k < 3; ++k) {
+			temp = state[0][k];
+			if (temp == state[1][k] && temp == state[2][k]) {
+				//if(temp != 0) cout << "横" << k << endl;
+				if (vict == 0) {
+					vict = temp;
+				}
+				else if (vict != temp && temp != 0) {
+					vict = 100;
+					break;
+				}
+			}
+			temp = state[k][0];
+			if (temp == state[k][1] && temp == state[k][2]) {
+				//if(temp != 0) cout << "縦" << k << endl;
+				if (vict == 0) {
+					vict = temp;
+				}
+				else if (vict != temp && temp != 0) {
+					vict = 100;
+					break;
+				}
+			}
+		}
+		temp = state[0][0];
+		if (temp == state[1][1] && temp == state[2][2]) {
+			//if (temp != 0) cout << "左斜め" << endl;
+			if (vict == 0) {
+				vict = temp;
+			}
+			else if (vict != temp && temp != 0) {
+				vict = 100;
+			}
+		}
+		temp = state[2][0];
+		if (temp == state[1][1] && temp == state[0][2]) {
+			//if (temp != 0) cout << "右斜め" << endl;
+			if (vict == 0) {
+				vict = temp;
+			}
+			else if (vict != temp && temp != 0) {
+				vict = 100;
+			}
+		}
+		if (filled() == 1 && vict == 0) vict = 10;
+		return vict;
+	} // 0:on-game, 1:Black, -1:White, 10: Draw, 100:Error
+
+	int update(int i, int j, int side) {
+		if (victory() != 0) return -1;
+		if (state[i][j] == 0) {
+			state[i][j] = side;
+			return 0;
+		}
+		return -1;
+	}
+
+	Field operator = (const Field& src) {
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				state[i][j] = src.state[i][j];
+			}
+		}
+		return src;
+	}
+};
+
+
+class Game {
+private:
+	// 定数
+	unsigned int White = GetColor(255, 255, 255);
+	unsigned int Black = GetColor(0, 0, 0);
+
+public:
+	int cnt = 0;		// ターン数
+	int nextField = -1;		// 次の盤面、-1: anywhere
+
+	// 盤面上の操作関連
+	int globalX = 1;
+	int globalY = 1;
+	int localX = 1;
+	int localY = 1;			//キーボード操作時の座標
+
+	Field mother;
+	Field child[3][3];
+	Mouse mouse;
+
+	Game() {
+		initialize();
+	}
+
+	void initialize() {
+		cnt = 0;
+		nextField = -1;
+		mother.initialize();
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				child[i][j].initialize();
+			}
+		}
+	}
+
+
+	/*===========================*/
+	//    盤面表示関連
+	/*===========================*/
+	void drawBase() {
+		DrawBox(160, 80, 460, 380, White, TRUE);
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				DrawBox(160 + 100 * i, 80 + 100 * j, 160 + 100 * (i + 1), 80 + 100 * (j + 1), Black, FALSE);
+			}
+		}
+	}
+
+	void drawNextField() {
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				if (nextField == 3 * i + j) {
+					for (int w = -1; w <= 1; ++w) {
+						int upLeftX = 160 + 100 * i + w;
+						int upLeftY = 80 + 100 * j + w;
+						int lowRightX = 160 + 100 * (i + 1) - w;
+						int lowRightY = 80 + 100 * (j + 1) - w;
+						DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
+					}
+				}
+				else if (nextField == -1 && child[i][j].victory() == 0) {
+					int upLeftX = 160 + 100 * i;
+					int upLeftY = 80 + 100 * j;
+					int lowRightX = 160 + 100 * (i + 1);
+					int lowRightY = 80 + 100 * (j + 1);
+					DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
+				}
+			}
+		}
+	}
+
+	void drawCurrentCoord() {
+		int upLeftX = 160 + 100 * globalX + 33 * localX;
+		int upLeftY = 80 + 100 * globalY + 33 * localY;
+		int lowRightX = 160 + 100 * globalX + 33 * (localX + 1) + 1;
+		int lowRightY = 80 + 100 * globalY + 33 * (localY + 1) + 1;
+		DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
+	}
+
+
+	/*===========================*/
+	//    盤面更新関連
+	/*===========================*/
+	void getMouseCoord() {
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				for (int k = 0; k < 3; ++k) {
+					for (int l = 0; l < 3; ++l) {
+						int upLeftX = 160 + 100 * i + 33 * k;
+						int upLeftY = 80 + 100 * j + 33 * l;
+						int lowRightX = 160 + 100 * i + 33 * (k + 1);
+						int lowRightY = 80 + 100 * j + 33 * (l + 1);
+						if (mouse.onButton(upLeftX, upLeftY, lowRightX, lowRightY)) {
+							globalX = i;
+							globalY = j;
+							localX = k;
+							localY = l;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	bool playTurn() {
+		getMouseCoord();
+		if (mouse.clickRight()) {
+			return true;
+		}
+		return false;
+	}
+
+	double update(int side = 0) {
+		return update(globalX, globalY, localX, localY, side);
+	}
+
+	double update(int global_x, int global_y, int local_x, int local_y, int side = 0) {
+		// sideが指定されていなければ、cntから計算する
+		if (side == 0) {
+			side = 1 - 2 * (cnt % 2);
+		}
+		//盤面の更新
+		if (nextField == 3 * global_x + global_y || nextField == -1) {
+			if (child[global_x][global_y].update(local_x, local_y, side) == 0) {
+				cnt++;
+				//全体の更新
+				mother.update(global_x, global_y, child[global_x][global_y].victory());
+				if (child[local_x][local_y].victory() != 0) {
+					nextField = -1;
+					return 0;
+				}
+				else {
+					nextField = local_x * 3 + local_y;
+					return 0;
+				}
+			}
+		}
+		return -100.0;
+	}
+};
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
