424ef6a561f090d1bfc34c6c86e7193f70c5232e
maxence-kuriyama <maxence.no1@gmail.com>
2021-09-06 01:27:47 +0900

    右クリック、Field削除、メモリ125MB使用

--- main.cpp
+++ main.cpp
@@ -25,10 +25,6 @@ public:
 		return 0;
 	}
 
-	bool clickRight() {
-		return button[1] == 1;
-	}
-
 	//矩形の上にマウスカーソルがいるか判定
 	bool onButton(int LeftUpx, int LeftUpy, int RightDownx, int RightDowny) {
 		return (x > LeftUpx && x < RightDownx&& y > LeftUpy && y < RightDowny);
@@ -36,101 +32,21 @@ public:
 };
 
 
-class Field {
-public:
-	int state[3][3];	// 0:None, 1:Black, -1:White
-
-	Field() {
-		initialize();
-	}
-
-	void initialize() {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				state[i][j] = 0;
-			}
-		}
-	}
-
-	int update(int i, int j, int side) {
-		if (state[i][j] == 0) {
-			state[i][j] = side;
-			return 0;
-		}
-		return -1;
-	}
-
-	Field operator = (const Field& src) {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				state[i][j] = src.state[i][j];
-			}
-		}
-		return src;
-	}
-};
-
-
 class Game {
-private:
-	// 定数
+public:
 	unsigned int White = GetColor(255, 255, 255);
 	unsigned int Black = GetColor(0, 0, 0);
-
-public:
-	int cnt = 0;		// ターン数
-	int nextField = -1;		// 次の盤面、-1: anywhere
-
-	// 盤面上の操作関連
 	int globalX = 1;
 	int globalY = 1;
 	int localX = 1;
-	int localY = 1;			//キーボード操作時の座標
-
-	Field mother;
-	Field child[3][3];
+	int localY = 1;
 	Mouse mouse;
 
-	Game() {
-		initialize();
-	}
-
-	void initialize() {
-		cnt = 0;
-		nextField = -1;
-		mother.initialize();
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				child[i][j].initialize();
-			}
-		}
-	}
-
-
-	/*===========================*/
-	//    盤面表示関連
-	/*===========================*/
 	void drawBase() {
 		DrawBox(160, 80, 460, 380, White, TRUE);
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
 				DrawBox(160 + 100 * i, 80 + 100 * j, 160 + 100 * (i + 1), 80 + 100 * (j + 1), Black, FALSE);
-			}
-		}
-	}
-
-	void drawNextField() {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				if (nextField == 3 * i + j) {
-					for (int w = -1; w <= 1; ++w) {
-						int upLeftX = 160 + 100 * i + w;
-						int upLeftY = 80 + 100 * j + w;
-						int lowRightX = 160 + 100 * (i + 1) - w;
-						int lowRightY = 80 + 100 * (j + 1) - w;
-						DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
-					}
-				}
 			}
 		}
 	}
@@ -143,10 +59,6 @@ public:
 		DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
 	}
 
-
-	/*===========================*/
-	//    盤面更新関連
-	/*===========================*/
 	void getMouseCoord() {
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
@@ -167,35 +79,6 @@ public:
 			}
 		}
 	}
-
-	bool playTurn() {
-		getMouseCoord();
-		if (mouse.clickRight()) {
-			return true;
-		}
-		return false;
-	}
-
-	double update(int side = 0) {
-		return update(globalX, globalY, localX, localY, side);
-	}
-
-	double update(int global_x, int global_y, int local_x, int local_y, int side = 0) {
-		// sideが指定されていなければ、cntから計算する
-		if (side == 0) {
-			side = 1 - 2 * (cnt % 2);
-		}
-		//盤面の更新
-		if (nextField == 3 * global_x + global_y || nextField == -1) {
-			if (child[global_x][global_y].update(local_x, local_y, side) == 0) {
-				cnt++;
-				//全体の更新
-				nextField = local_x * 3 + local_y;
-				return 0;
-			}
-		}
-		return -100.0;
-	}
 };
 
 
@@ -214,20 +97,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	Game game;
 
-	unsigned int White = GetColor(255, 255, 255);
-	unsigned int Black = GetColor(0, 0, 0);
-
 	//メインループ
 	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen()) {
 		// 入力情報を取得
 		game.mouse.update();
 
 		game.drawBase();
-		game.drawNextField();
-		// プレイヤーの操作
-		if (game.playTurn()) {
-			game.update();
-		}
+		game.getMouseCoord();
 		game.drawCurrentCoord();
 	}
 
@@ -236,4 +112,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	return 0;
 }
-
