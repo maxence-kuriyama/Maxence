a7c7c11860113bc4d40565c3db3f24914a0d60bf
maxence-kuriyama <maxence.no1@gmail.com>
2021-09-06 01:31:17 +0900

    Mouse削除、メモリ125MB使用

--- main.cpp
+++ main.cpp
@@ -2,36 +2,6 @@
 
 using namespace DxLib;
 
-class Mouse {
-public:
-	unsigned int button[8];
-	int x;
-	int y;
-
-	//マウス状態の取得
-	int update() {
-		if (GetMousePoint(&x, &y) == -1) {
-			return -1;
-		}
-		int MouseInput = GetMouseInput();
-		for (int i = 0; i < 8; i++) {
-			if (MouseInput & (1 << i)) {
-				button[i]++;
-			}
-			else {
-				button[i] = 0;
-			}
-		}
-		return 0;
-	}
-
-	//矩形の上にマウスカーソルがいるか判定
-	bool onButton(int LeftUpx, int LeftUpy, int RightDownx, int RightDowny) {
-		return (x > LeftUpx && x < RightDownx&& y > LeftUpy && y < RightDowny);
-	}
-};
-
-
 class Game {
 public:
 	unsigned int White = GetColor(255, 255, 255);
@@ -40,7 +10,6 @@ public:
 	int globalY = 1;
 	int localX = 1;
 	int localY = 1;
-	Mouse mouse;
 
 	void drawBase() {
 		DrawBox(160, 80, 460, 380, White, TRUE);
@@ -58,56 +27,27 @@ public:
 		int lowRightY = 80 + 100 * globalY + 33 * (localY + 1) + 1;
 		DrawBox(upLeftX, upLeftY, lowRightX, lowRightY, Black, FALSE);
 	}
-
-	void getMouseCoord() {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				for (int k = 0; k < 3; ++k) {
-					for (int l = 0; l < 3; ++l) {
-						int upLeftX = 160 + 100 * i + 33 * k;
-						int upLeftY = 80 + 100 * j + 33 * l;
-						int lowRightX = 160 + 100 * i + 33 * (k + 1);
-						int lowRightY = 80 + 100 * j + 33 * (l + 1);
-						if (mouse.onButton(upLeftX, upLeftY, lowRightX, lowRightY)) {
-							globalX = i;
-							globalY = j;
-							localX = k;
-							localY = l;
-						}
-					}
-				}
-			}
-		}
-	}
 };
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	SetOutApplicationLogValidFlag(FALSE);
 	ChangeWindowMode(TRUE);
-	SetAlwaysRunFlag(TRUE);
 	SetMainWindowText("Maxence 0.4.0");
-	SetWindowIconID(101);
 	if (DxLib_Init() == -1) {
 		return -1;
 	}
 	SetDrawScreen(DX_SCREEN_BACK);
 
-
 	Game game;
 
 	//メインループ
 	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen()) {
-		// 入力情報を取得
-		game.mouse.update();
 
 		game.drawBase();
-		game.getMouseCoord();
 		game.drawCurrentCoord();
 	}
 
-	InitGraph();
 	DxLib_End();
 
 	return 0;
