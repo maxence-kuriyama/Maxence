6cce2969b3b62bab837e9aa139f98077b58861b2
maxence-kuriyama <maxence.no1@gmail.com>
2025-07-21 19:29:54 +0900

    試験的にフェードインカットイン実装

--- lib/components/anime/fade_cutin.h
+++ lib/components/anime/fade_cutin.h
@@ -3,11 +3,19 @@
 
 #include "lib/components/anime.h"
 
+const int FADE_CUTIN_RIGHT_CHARA_X = 400;
+const int FADE_CUTIN_RIGHT_CHARA_Y = 50;
+
 // 試合中のアニメーションクラス
 class FadeCutin : public AnimeBase {
 private:
 	int image0 = NULL;
 	int image1 = NULL;
+	int Black = GetColor(0, 0, 0);
+
+	int alpha = 0;
+	int x = FADE_CUTIN_RIGHT_CHARA_X;
+	int y = FADE_CUTIN_RIGHT_CHARA_Y;
 
 public:
 
@@ -16,21 +24,23 @@ public:
 		image1 = LoadGraph("graph/cutin2_1.png");
 		GraphBlend(image0, image1, 255, DX_GRAPH_BLEND_MULTIPLE);
 
-		AnimeBase::initialize();
+		initialize();
 	}
 
 	void update() {
 		if (!active) return;
 
 		if (cnt <= 15) {
-			slideIn();
+			alpha += 10;
 		}
 		else if (cnt <= 105) {
-			display();
+			// do nothing
 		}
 		else if (cnt <= 120) {
-			slideOut();
+			alpha -= 10;
 		}
+		x -= 1;
+		display();
 		cnt++;
 
 		if (cnt > 120) initialize();
@@ -38,27 +48,27 @@ public:
 
 private:
 
-	void slideIn() {
-		SetDrawBlendMode(DX_BLENDMODE_SUB, 255);
-		DrawGraph(pow(cnt - 15, 2.0) * 640 / pow(15.0, 2.0), 0, image1, FALSE);
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 225);
-		DrawGraph(pow(cnt - 15, 2.0) * 640 / pow(15.0, 2.0), 0, image0, FALSE);
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	void initialize() {
+		AnimeBase::initialize();
+
+		alpha = 0;
+		x = FADE_CUTIN_RIGHT_CHARA_X;
+		y = FADE_CUTIN_RIGHT_CHARA_Y;
 	}
 
 	void display() {
-		SetDrawBlendMode(DX_BLENDMODE_SUB, 255);
-		DrawGraph(0, 0, image1, FALSE);
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 225);
-		DrawGraph(0, 0, image0, FALSE);
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-	}
+		int screenHandle = MakeScreen(640, 480, TRUE);
 
-	void slideOut() {
-		SetDrawBlendMode(DX_BLENDMODE_SUB, 255);
-		DrawGraph(-pow(cnt - 105, 2.0) * 640 / pow(15.0, 2.0), 0, image1, FALSE);
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 225);
-		DrawGraph(-pow(cnt - 105, 2.0) * 640 / pow(15.0, 2.0), 0, image0, FALSE);
+		SetDrawScreen(screenHandle);
 		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+		DrawBox(-1, -1, 641, 481, Black, TRUE);
+		DrawExtendGraph(x, y, x + 400, y + 400, image1, TRUE);
+
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, alpha);
+		SetDrawScreen(DX_SCREEN_BACK);
+		DrawGraph(0, 0, screenHandle, TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+		DeleteGraph(screenHandle);
 	}
 };
